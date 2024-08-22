005f680b6b10f866786fa1855bdb0d6edf3482ab
Jayash Saini <jayashysaini7361@gmail.com>
2024-08-23 02:13:55 +0530

    feat(like): improve like section

--- src/components/Courses/AboutCourse.tsx
+++ src/components/Courses/AboutCourse.tsx
@@ -2,21 +2,41 @@ import React, { useEffect, useState } from "react";
 import { AiOutlineLike, AiFillLike } from "react-icons/ai";
 import { CourseLessonInterface } from "../../interfaces";
 import { requestHandler } from "../../util";
-import { toggleCourseLikeHandler } from "../../api";
+import { getSyllabusById, toggleCourseLikeHandler } from "../../api";
+import { toast } from "sonner";
+import { TailSpin } from "react-loader-spinner";
 
 interface AboutCourseProps {
-  lesson?: CourseLessonInterface;
   about?: string;
+  courseId?: string;
+  lessonId?: string;
 }
-const AboutCourse: React.FC<AboutCourseProps> = ({ lesson, about }) => {
-  const [isUserLiked, setIsUserLiked] = useState(lesson?.isUserLiked);
-  const [totalLikes, setTotalLikes] = useState<number | undefined>(
-    lesson?.totalLikes || 0
-  );
+const AboutCourse: React.FC<AboutCourseProps> = ({
+  courseId,
+  lessonId,
+  about,
+}) => {
+  const [lesson, setLesson] = useState<CourseLessonInterface>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUserLiked, setIsUserLiked] = useState(false);
+  const [totalLikes, setTotalLikes] = useState<number | undefined>(0);
+  useEffect(() => {
+    requestHandler(
+      async () => await getSyllabusById(courseId || "", lessonId || ""),
+      setIsLoading,
+      ({ data }) => {
+        setLesson(data);
+      },
+      (err) => {
+        toast.error(err);
+        window.location.href = "/";
+      }
+    );
+  }, []);
 
   useEffect(() => {
-    setIsUserLiked(lesson?.isUserLiked);
-    setTotalLikes(lesson?.totalLikes);
+    setIsUserLiked(lesson?.isUserLiked || false);
+    setTotalLikes(lesson?.totalLikes || 0);
   }, [lesson]);
 
   const handleLikeToggle = async () => {
@@ -34,7 +54,20 @@ const AboutCourse: React.FC<AboutCourseProps> = ({ lesson, about }) => {
     );
   };
 
-  return (
+  return isLoading ? (
+    <div className="w-full h-[500px] flex justify-center items-center custom-main-bg">
+      <TailSpin
+        visible={true}
+        height="30"
+        width="30"
+        color="#fff"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
+  ) : (
     <>
       <h2 className="sm:text-2xl text-base font-medium">{lesson?.title}</h2>
       <p className="text-sm font-light mt-1 custom-color">
